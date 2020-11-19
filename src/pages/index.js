da8273f2021a7d2b0ba5d70b0101fcb7b19bc581
paolaozv <paolaozv@gmail.com>
2020-11-19 21:43:57 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+import Layout from '../components/Layout/Layout';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <Layout>main</Layout>
+  )
+};

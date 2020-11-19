a5d6d8b81824517bb828586f37804e501b4831a4
paolaozv <paolaozv@gmail.com>
2020-11-19 22:14:26 +0100

    add favicon and get data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,21 @@ import Head from 'next/head';
 import Layout from '../components/Layout/Layout';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
   return (
     <Layout>main</Layout>
   )
 };
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await response.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
+

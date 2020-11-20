55ad167715f28522331d1021bb8afdfc8c0128c2
paolaozv <paolaozv@gmail.com>
2020-11-20 15:51:20 +0100

    add components like CountriesTable, SearchInput, add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head';
+import CountriesTable from '../components/CountriesTable/CountriesTable';
 import Layout from '../components/Layout/Layout';
+import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css';
 
 export default function Home({ countries }) {
   console.log(countries);
   return (
-    <Layout>main</Layout>
+    <Layout>
+      <div className={styles.counts}>
+        Found {countries.length} countries
+      </div>
+      <SearchInput placeholder='Filter by Name, Region or Subregion' />
+      <CountriesTable countries={countries} />
+    </Layout>
   )
 };
 
